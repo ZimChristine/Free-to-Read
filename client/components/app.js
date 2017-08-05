42ee9c29a1ac2f112891c8ad79224335c709674d
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-08-05 11:48:35 -0700

    get server connected to db

--- client/components/app.js
+++ client/components/app.js
@@ -1,4 +1,10 @@
-angular.module('reading-list', () => { //do I need to initialize with [] as second argument here?
-  .controller();
+angular.module('reading') //do I need to initialize with [] as second argument here?
 
-});
+  .controller('AppCtrl', () => {
+    this.purple = 'purple is NICCCCCCE!';
+  })
+  .component('app', {
+    templateUrl: '../templates/app.html',
+    controller: 'AppCtrl'
+  });
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
-var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var express = require('express');
 var app = express();
 
 var port = 8080;
@@ -8,13 +9,30 @@ var ip = '127.0.0.1';
 
 
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(__dirname + '/client'));
 
-app.get('/', function(request, response) {
-  // render index.html page
-  //response.sendStatus(200);
-  response.sendFile(path.join(__dirname, 'client/index.html'));
-})
+mongoose.connect('mongodb://127.0.0.1/list', {
+  useMongoClient: true,
+});
+var db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.log('Connection error!!!!!!!!!!!!!', error);
+});
+
+db.once('open', () => {
+  console.log('Mongo working!');
+});
+
+// db.then((db) => {
+
+// });
+
+// app.get('/', function(request, response) {
+//   // render index.html page
+//   //response.sendStatus(200);
+//   response.sendFile(path.join(__dirname, 'client/index.html'));
+// })
 
 app.listen(port, ip, function() {
   console.log('listening on: ', ip, ' port: ', port);
